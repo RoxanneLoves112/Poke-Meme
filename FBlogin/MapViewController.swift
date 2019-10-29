64b30550470e82d2a0acbe86e4fc44b37e455c14
Rox <chentinz@andrew.cmu.edu>
2019-10-28 21:27:11 -0400

    implemented actionSheet & camera or image picker

--- FBlogin/MapViewController.swift
+++ FBlogin/MapViewController.swift
@@ -13,7 +13,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
   
   @IBOutlet weak var mapView: MKMapView!
   var profilePhoto: UIImage?
-  
+  var imagePicker = UIImagePickerController()
   let location = Location()
   
   override func viewDidLoad() {
@@ -28,6 +28,49 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     droppedPin.title = "You Are Here"
     droppedPin.subtitle = "Look it's you!"
     mapView.addAnnotation(droppedPin)
+  }
+  
+  @IBAction func displayActionSheet(_ sender: Any) {
+      let optionMenu = UIAlertController(title: nil, message: "Choose Meme Image", preferredStyle: .actionSheet)
+    
+      let cameraAction = UIAlertAction(title: "Take Photo", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+          self.openCamera()
+      })
+      let albumAction = UIAlertAction(title: "Choose from Album", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+        self.openGallery()
+      })
+      let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+      optionMenu.addAction(cameraAction)
+      optionMenu.addAction(albumAction)
+      optionMenu.addAction(cancelAction)
+          
+      self.present(optionMenu, animated: true, completion: nil)
+  }
+  
+  func openCamera()
+  {
+    if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+      {
+        imagePicker.sourceType = UIImagePickerController.SourceType.camera
+          imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+          self.present(imagePicker, animated: true, completion: nil)
+      }
+      else
+      {
+          let alert  = UIAlertController(title: "Warning", message: "You don't have camera. Camera is not available in simulators.", preferredStyle: .alert)
+          alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+          self.present(alert, animated: true, completion: nil)
+      }
+  }
+
+  func openGallery()
+  {
+    imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+      imagePicker.allowsEditing = true
+    imagePicker.delegate = self
+      self.present(imagePicker, animated: true, completion: nil)
   }
   
   let regionRadius: CLLocationDistance = 400
@@ -49,7 +92,16 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     pinView!.canShowCallout = true
     return pinView
   }
-  
 }
 
-
+extension MapViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.isNavigationBarHidden = false
+        self.dismiss(animated: true, completion: nil)
+    }
+}
